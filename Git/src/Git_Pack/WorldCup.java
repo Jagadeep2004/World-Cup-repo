4edfa5ec38282528037afbf15ee8bfcec7e78e6b
jagadeep <jagadeep2004@gmail.com>
2026-03-04 14:26:39 +0530

    India won successfully

---
+++ Git/src/Git_Pack/WorldCup.java
@@ -0,0 +1,10 @@
+package Git_Pack;
+
+public class WorldCup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("India won the world cup");
+	}
+
+}
